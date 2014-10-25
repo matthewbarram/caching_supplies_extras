9272c8e3b6c92e6d3c5e095689a560c0360a1692
matthewbarram <matthewbarram@gmail.com>
2014-10-26 07:33:42 +1000

    Adding number incrementing for GST JS

---
+++ change_numbers.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+
+  var $pricesArray = $(".sqs-money-native");
+
+  function getNewTotal(pricesArray, i){
+    var oldTotal = $($pricesArray[i]).text();
+    var integerAmount = parseFloat(oldTotal);
+    var newTotal = integerAmount * 1.1;
+    var finalFloat = parseFloat(newTotal).toFixed(2);
+    return finalFloat;
+  };
+
+  function addGstToPrice(){
+    for (var i = 0; i < $pricesArray.size(); i++) {
+      $($(".sqs-money-native")[i]).html(getNewTotal($pricesArray, i));
+    };
+  };
+
+
+  $.fn.exists = function () {
+    return this.length !== 0;
+  };
+
+  var $subtotalElement = $(".sqs-fullpage-shopping-cart-content > .cart-container > .subtotal >.label");
+
+  if ($subtotalElement.exists()){
+      $subtotalElement.html("Subtotal (excluding GST)");
+  }
+  addGstToPrice();
+});
