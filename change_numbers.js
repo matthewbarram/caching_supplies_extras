60aae6b16663a80a8f4f54da65056540c127bbb6
matthewbarram <matthewbarram@gmail.com>
2014-12-18 21:22:41 +1000

    Added ability for pricing to update when item is changed.

--- change_numbers.js
+++ change_numbers.js
@@ -1,33 +1,62 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>
 $( document ).ready(function() {
-
-  var $prices = $(".sqs-money-native");
-
-  function newTotal(prices, i){
-    var oldTotal = $($prices[i]).text();
+ 
+  var $pricesArray = $(".sqs-money-native");
+	
+  $(".sold-out").html("Coming Soon")
+  
+  function getNewTotal(pricesArray, i){
+    var oldTotal = $($pricesArray[i]).text();
     var integerAmount = parseFloat(oldTotal);
     var newTotal = integerAmount * 1.1;
     var finalFloat = parseFloat(newTotal).toFixed(2);
     return finalFloat;
   };
-
+ 
   function addGstToPrice(){
-    for (var i = 0; i < $prices.size(); i++) {
-      $($(".sqs-money-native")[i]).html(newTotal($pricesArray, i));
+    for (var i = 0; i < $pricesArray.size(); i++) {
+      $($(".sqs-money-native")[i]).html(getNewTotal($pricesArray, i));
     };
   };
-
+ 
+ 
   $.fn.exists = function () {
     return this.length !== 0;
   };
-
+ 
   var $subtotalElement = $(".sqs-fullpage-shopping-cart-content > .cart-container > .subtotal >.label");
-
+ 
+  var $subtotalPrice = $(".sqs-fullpage-shopping-cart-content > .cart-container > table > tr > td.price");
+ 
   if ($subtotalElement.exists()){
       $subtotalElement.html("Subtotal (excluding GST)");
+      $subtotalPrice.html("Prices (excludes GST)");
   }
-
   addGstToPrice();
+
+  $( ".product-variants" ).change(function() {
+
+    var $pricesArray = $(".sqs-money-native");
+	
+  
+    function getNewTotal(pricesArray, i){
+      var oldTotal = $($pricesArray[i]).text();
+      var integerAmount = parseFloat(oldTotal);
+      var newTotal = integerAmount * 1.1;
+      var finalFloat = parseFloat(newTotal).toFixed(2);
+      return finalFloat;
+    };
+ 
+    function addGstToPrice(){
+      for (var i = 0; i < $pricesArray.size(); i++) {
+        $($(".sqs-money-native")[i]).html(getNewTotal($pricesArray, i));
+      };
+    };
+
+    addGstToPrice();
+
+  });
 
 });
 </script>
