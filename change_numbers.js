88b6fd78bc3be96cdd77b1d38667eb67d5770922
matthewbarram <matthewbarram@gmail.com>
2014-12-21 17:28:18 +1000

    Fixed problem of adding price to cart

--- change_numbers.js
+++ change_numbers.js
@@ -1,11 +1,11 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>
 $( document ).ready(function() {
- 
+
   var $pricesArray = $(".sqs-money-native");
-	
+
   $(".sold-out").html("Coming Soon")
-  
+
   function getNewTotal(pricesArray, i){
     var oldTotal = $($pricesArray[i]).text();
     var integerAmount = parseFloat(oldTotal);
@@ -13,22 +13,22 @@ $( document ).ready(function() {
     var finalFloat = parseFloat(newTotal).toFixed(2);
     return finalFloat;
   };
- 
+
   function addGstToPrice(){
     for (var i = 0; i < $pricesArray.size(); i++) {
       $($(".sqs-money-native")[i]).html(getNewTotal($pricesArray, i));
     };
   };
- 
- 
+
+
   $.fn.exists = function () {
     return this.length !== 0;
   };
- 
+
   var $subtotalElement = $(".sqs-fullpage-shopping-cart-content > .cart-container > .subtotal >.label");
- 
+
   var $subtotalPrice = $(".sqs-fullpage-shopping-cart-content > .cart-container > table > tr > td.price");
- 
+
   if ($subtotalElement.exists()){
       $subtotalElement.html("Subtotal (excluding GST)");
       $subtotalPrice.html("Prices (excludes GST)");
@@ -38,8 +38,8 @@ $( document ).ready(function() {
   $( ".product-variants" ).change(function() {
 
     var $pricesArray = $(".sqs-money-native");
-	
-  
+
+
     function getNewTotal(pricesArray, i){
       var oldTotal = $($pricesArray[i]).text();
       var integerAmount = parseFloat(oldTotal);
@@ -47,11 +47,9 @@ $( document ).ready(function() {
       var finalFloat = parseFloat(newTotal).toFixed(2);
       return finalFloat;
     };
- 
+
     function addGstToPrice(){
-      for (var i = 0; i < $pricesArray.size(); i++) {
-        $($(".sqs-money-native")[i]).html(getNewTotal($pricesArray, i));
-      };
+      $($(".sqs-money-native")[0]).html(getNewTotal($pricesArray, 0));
     };
 
     addGstToPrice();
