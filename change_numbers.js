a81a4c4dd296135ab0944cec8610967339c528e4
matthewbarram <matthewbarram@gmail.com>
2015-09-22 19:30:25 +1000

    Updated jquery version

--- change_numbers.js
+++ change_numbers.js
@@ -1,4 +1,4 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script>
 $( document ).ready(function() {
 
