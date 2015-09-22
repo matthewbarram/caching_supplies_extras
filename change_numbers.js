e27ffe98d53ee62b540db7aca93bfd17f8946087
matthewbarram <matthewbarram@gmail.com>
2015-09-22 19:32:35 +1000

    Adding chat widget

--- change_numbers.js
+++ change_numbers.js
@@ -78,3 +78,5 @@ $( document ).ready(function() {
     emailonly: false
   });
 </script>
+
+<script type="text/javascript" data-cfasync="false">(function () { var done = false;var script = document.createElement('script');script.async = true;script.type = 'text/javascript';script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript';document.getElementsByTagName('HEAD').item(0).appendChild(script);script.onreadystatechange = script.onload = function (e) {if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {var w = new PCWidget({ c: '3583e936-7377-4b39-a0a2-c59e9d42d171', f: true });done = true;}};})();</script>
